17faba125d600b1abb2a43bf991fec8184038ccf
Oscar Lobaton <clauditanorato@gmail.com>
2025-02-26 10:11:06 -0500

    ramas malas

--- src/test/java/org/inlaze/stepdefinitions/StepErroresInicio.java
+++ src/test/java/org/inlaze/stepdefinitions/StepErroresInicio.java
@@ -21,7 +21,7 @@ public class StepErroresInicio {
         setTheStage(new OnlineCast());
     }
 
-    //revision no quiero que cambien esto por favor
+    //revision no quiero que cambien esto por favor hol
     @Cuando("proporciona su {string} y {string}")
     public void provideYourEmailAndPassword(String email, String password) {
             log.info("Que llego {}", email);
